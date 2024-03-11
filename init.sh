019d96ffcdd4549e4e39b99e70c9ed2c90eacd83
Chris Vann <chris@vann.pro>
2024-03-11 00:16:04 -0500

    remove -nf in case important file exists

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@ DOTFILES=$(dirname "$DOTFILES")
 
 echo "Using dotfiles from $DOTFILES"
 
-ln -nfs "$DOTFILES/bash/.bash_aliases" ~/.bash_aliases
-ln -nfs "$DOTFILES/git/.gitconfig" ~/.gitconfig
-ln -nfs "$DOTFILES/vim/.vimrc" ~/.vimrc
-ln -nfs "$DOTFILES/vim/" ~/.vim
+ln -s "$DOTFILES/bash/.bash_aliases" ~/.bash_aliases
+ln -s "$DOTFILES/git/.gitconfig" ~/.gitconfig
+ln -s "$DOTFILES/vim/.vimrc" ~/.vimrc
+ln -s "$DOTFILES/vim/" ~/.vim
 
 echo "Dotfiles installed to $(realpath ~/)"
