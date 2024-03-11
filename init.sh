fffe05a1e2230f06f95ed8e8826c94480171c63f
Chris Vann <chris@vann.pro>
2024-03-10 23:56:36 -0500

    add gitconfig

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ DOTFILES=$(dirname "$DOTFILES")
 echo "Using dotfiles from $DOTFILES"
 
 ln -nfs "$DOTFILES/bash/.bash_aliases" ~/.bash_aliases
+ln -nfs "$DOTFILES/git/.gitconfig" ~/.gitconfig
 ln -nfs "$DOTFILES/vim/.vimrc" ~/.vimrc
 ln -nfs "$DOTFILES/vim/" ~/.vim
 
